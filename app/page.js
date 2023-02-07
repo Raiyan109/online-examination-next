0726da7e0b8f56a8e5e56a129b5e2ecb5c3b0a1c
Raiyan109 <kraiyan109@gmail.com>
2023-02-07 20:36:41 +0600

    page styles removed

--- app/page.js
+++ app/page.js
@@ -8,9 +8,15 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-      <nav style={{ backgroundColor: 'blue', width: '100%', height: '60px' }}>
-        <h1 style={{ textTransform: 'uppercase', }}>Demo online test</h1>
+      <nav style={{ backgroundColor: 'purple', width: '100%', height: '60px' }}>
+        <h1 style={{ textTransform: 'uppercase', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Demo online test</h1>
       </nav>
+
+      {/* Buttons */}
+      <button style={{ backgroundColor: 'blue', padding: '10px', outline: 'none' }}>All Sections</button>
+      <button style={{ backgroundColor: 'skyblue', padding: '10px' }}>Physics</button>
+      <button style={{ backgroundColor: 'skyblue', padding: '10px' }}>Chemistry</button>
+      <button style={{ backgroundColor: 'skyblue', padding: '10px' }}>Maths</button>
     </main>
   )
 }
